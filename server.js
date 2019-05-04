424239cd2b31148e361ace09092c01c31459e487
alex chalyy <alexchalyy@gmail.com>
2019-05-04 18:54:54 -0400

    when a menu item is clicked, it is not displayed on the next page:(

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// Written by Alex Chalyy, Ron Seide, Tori Farfak, and David Lawrence on 5/4/2019.
+//
+// Description: 
+//
+// This is deli order web site:
+//
+// 1.	User is navigated to index welcome page with menu and orders page 	links. If user clicks on menu, page in next step is opened. If user clicks 	orders, page from step 3 is opened.
+//
+// 2.	User selects the food item they would like to order from menu and 	clicks order.
+//
+// 3.	Afterwards, user is navigated to next page where queue of orders is 	displayed with food items that were ordered, price, index, and 	estimated time when the food would be ready (minutes) in processed 	column. 
+//
+// 4.	Every time the page is refreshed, the food item would be moved from 	processed column to ready for pick up column, if enough time has 	passed from its order time. There is also a pick up button in that 	column. If that button is clicked, order is removed from the page.
+
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
