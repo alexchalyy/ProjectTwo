48956c030a42628f97e96d8e6b681e13714de59d
David Lawrence <dalaw85@gmail.com>
2019-05-05 15:40:13 -0400

    landing page and menu basic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,15 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    // I commented this out because our rendering of information from the database will be shown on our "order status page", but will clean this up once we 
+    // use this boiler plate code for rendering the data needed.
+    // db.Example.findAll({}).then(function(dbExamples) {                   
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    res.render("index");
   });
 
   // Load example page and pass in an example by id
@@ -18,6 +21,11 @@ module.exports = function(app) {
         example: dbExample
       });
     });
+  });
+  
+  // render the menu page
+  app.get("/menu", function(req, res) {
+    res.render("menu");
   });
 
   // Render 404 page for any unmatched routes
