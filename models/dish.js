85e71645493fe68611f20025280b0fd61d84bfb1
Tori <victoria.fafrak@gmail.com>
2019-05-05 20:01:15 -0400

    added column values to dish.js model

--- models/dish.js
+++ models/dish.js
@@ -1,7 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var Dish = sequelize.define("Dish", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    order: DataTypes.STRING,
+    ready: {type: DataTypes.BOOLEAN, defaultValue: false},
+    pickup: {type: DataTypes.BOOLEAN, defaultValue: false}
+    // id and time created are created by Sequelize
   });
   return Dish;
 };
