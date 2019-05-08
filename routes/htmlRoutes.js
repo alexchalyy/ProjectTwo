c5ccc9d6fc5d1322e98f311b7eb3a00a1d686005
David Lawrence <dalaw85@gmail.com>
2019-05-07 20:41:13 -0400

    render menu page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,13 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/menu", function(req, res) {
-    db.Dish.findAll({}).then(function(dbDishes) {
-      res.render("index", {
-        msg: "Welcome!",
-        dishes: dbDishes
-      });
-    });
+  app.get("/", function(req, res) {
+    // db.Dish.findAll({}).then(function(dbDishes) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     dishes: dbDishes
+    //   });
+    // });
   });
 
   app.get("/", function(req, res) {
