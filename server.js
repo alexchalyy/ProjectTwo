a710af78da5248b0728b96e13262537d2915ef5b
alex chalyy <alexchalyy@gmail.com>
2019-05-13 21:40:17 -0400

    modified server.js

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// db.sequelize.sync(syncOptions).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
